96ba5e3312f5544c9a79a93785e2230d6ff8fc51
Michael <tvmichael@meta.ua>
2017-08-07 13:25:31 +0300

    part

---
+++ Lesson3/part_4.py
@@ -0,0 +1,28 @@
+# part 4
+
+name_x = ['Mikola', 'David', 'Oleg', 'Rostislav', 'Kevin', 'Tomash']
+for nx in name_x:
+    print(nx)
+
+# range
+print('\n-- range')
+for i in range(4, 9):
+    print(i, end=' ')
+print()
+for i in range(1, 20, 2): # range(start, end, step)
+    print(i, end=' ')
+print()
+
+#
+print('\n-- list')
+numbers =list(range(7, 20))
+print(numbers)
+
+# for
+print('\n-- for .. list')
+num_list = [value**2 for value in range(1, 9)]
+print(num_list)
+num_list = [value+3 for value in range(4, 12)]
+print(num_list)
+num_list = [value/2 for value in range(2, 11)]
+print(num_list)
